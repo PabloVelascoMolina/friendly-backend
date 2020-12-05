8eea6c64fb0438908028287db2f2beb3bfff4eef
Pablo <paablovelasco@gmail.com>
2020-12-05 20:47:21 +0100

    Bug fixed 3

--- app/Http/Resources/Posts.php
+++ app/Http/Resources/Posts.php
@@ -18,7 +18,7 @@ class Posts extends JsonResource
             'id' => $this->id,
             'description' => $this->description,
             'category' => $this->category,
-            'image' => $this->image !== 'null' ? $this->image : null,
+            'image' => $this->image,
             'user_id' => $this->user_id,
             'created_at' => $this->created_at->format('d/m/Y'),
             'updated_at' => $this->updated_at->format('d/m/Y'),
