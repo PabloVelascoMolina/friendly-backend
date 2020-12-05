3296bc305cebbd6b86380585f8aa09bbaeb59c0d
Pablo <paablovelasco@gmail.com>
2020-12-05 03:54:33 +0100

    Bug fixed ip

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -60,13 +60,32 @@ class RegisterController extends BaseController
             $success['email'] =  $user->email;
             $success['id'] =  $user->id;
             $success['avatar'] =  $user->avatar;
-            $success['ip'] =  $_SERVER['REMOTE_ADDR'];
+            $success['ip'] =  $this->getRealIP();
 
             return $this->sendResponse($success, 'User login successfully.');
         } else {
             return $this->sendError('Unauthorised.', ['error' => 'Unauthorised']);
         }
     }
+
+    public function getRealIP()
+    {
+
+        if (isset($_SERVER["HTTP_CLIENT_IP"])) {
+            return $_SERVER["HTTP_CLIENT_IP"];
+        } elseif (isset($_SERVER["HTTP_X_FORWARDED_FOR"])) {
+            return $_SERVER["HTTP_X_FORWARDED_FOR"];
+        } elseif (isset($_SERVER["HTTP_X_FORWARDED"])) {
+            return $_SERVER["HTTP_X_FORWARDED"];
+        } elseif (isset($_SERVER["HTTP_FORWARDED_FOR"])) {
+            return $_SERVER["HTTP_FORWARDED_FOR"];
+        } elseif (isset($_SERVER["HTTP_FORWARDED"])) {
+            return $_SERVER["HTTP_FORWARDED"];
+        } else {
+            return $_SERVER["REMOTE_ADDR"];
+        }
+    }
+
 
     public function user(Request $request) {
         $user = Auth::user();
